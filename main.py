ffc2d50b16fd5d2e7be6a97fac04132c768fa026
timeodlc <delcroixtimeo92@gmail.com>
2024-09-27 11:43:07 +0200

    Update main.py
    Base du projet sur main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
+    pass
+    target_number = None
+    return target_number
+
+from utils import *
+
+def transforme ():
+    init_number = ask_for_the_init_number ()
+    init_base = ask_for_the_init_base ()
+    target_base = ask_for_the_target_base ()
+    target_number = \
+        bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base)
+
+transforme ()
+
+assert bin_dec_hex__to__bin_dec_hex (101, 2, 10) == 5
