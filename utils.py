57d5b6386d1ae53ad0a40dd3086ed63d50382b80
timeodlc <delcroixtimeo92@gmail.com>
2024-09-30 14:09:35 +0200

    Update utils.py
    update

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+bin_number_valid_chars = ["0", "1"]
+
+dec_number_valid_chars = \
+    bin_number_valid_chars \
+  + ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+hex_number_valid_chars = \
+    dec_number_valid_chars \
+  + ["A", "B", "C", "D", "E", "F"] \
+  + ["a", "b", "c", "d", "e", "f"]
+
 
 def check_char_number_validity (char):
     return char in hex_number_valid_chars
