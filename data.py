87cc5fca3ca0c17a277080d88f670a1421303a70
timeodlc <delcroixtimeo92@gmail.com>
2024-10-04 12:10:34 +0200

    advance version

--- data.py
+++ data.py
@@ -2,3 +2,22 @@ ask_for_the_init_number_text = "Entrez le nombre : "
 
 ask_again_for_the_init_number_text = "Entrez le nombre correct : "
 
+ask_for_the_init_base_text = "Entrez la base : "
+ask_again_for_the_init_base_text = \
+    "Entrez la base correctement !" \
+    + "\nSi vous souhaitez du binaire, tapez 2" \
+    + "\nSi vous souhaitez du décimal, tapez 10" \
+    + "\nSi vous souhaitez du hexadécimale, tapez 16  :  "
+
+bin_number_valid_chars = ["0", "1"]
+
+dec_number_valid_chars = \
+    bin_number_valid_chars \
+  + ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+hex_number_valid_chars = \
+    dec_number_valid_chars \
+  + ["A", "B", "C", "D", "E", "F"] \
+  + ["a", "b", "c", "d", "e", "f"]
+
+valid_base_strings = ["2", "10", "16"]
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ask_for_the_target_base () :
 
 def transform  ():
     init_number = ask_for_the_init_number ()
-    init_base = ask_for_the_init_number ()
+    init_base = ask_for_the_init_base ()
     target_base = ask_for_the_target_base ()
     target_number = \
         bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base)
@@ -21,6 +21,6 @@ def transform  ():
 
 transform ()
 
-assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
+#assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
 # ask_for_the_init_number 144
