086711852671f5e35e960e3c444f0daec605590b
member2013 <salomeruiz2008@yahoo.com>
2024-10-18 10:26:06 +0200

    FROM_... + def bin to dec, dec to bin, hex to dec et dec to hex

--- main.py
+++ main.py
@@ -12,55 +12,64 @@ def transform  ():
     target_number = \
         bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base)
 
-def bin_to_bin (number):
+def from_bin_to_bin (number):
      return number
 
-def dec_to_dec (number):
+def from_dec_to_dec (number):
      return number
 
-def hex_to_hex (number):
+def from_hex_to_hex (number):
      return number
 
-def hex_to_bin (number):
-      number = hex_to_dec (number)
-      number = dec_to_bin (number)
+def from_hex_to_bin (number):
+      number = from_hex_to_dec (number)
+      number = from_dec_to_bin (number)
       return number
 
-def bin_to_hex (number) :
-       number = bin_to_dec (number)
-       number = dec_to_hex (number)
+def from_bin_to_hex (number) :
+       number = from_bin_to_dec (number)
+       number = from_dec_to_hex (number)
        return number
 
-def bin_to_dec (number) :
+def from_bin_to_dec (number) :
+       pass
+
+def from_dec_to_bin (number) :
+       pass
+
+def from_hex_to_dec (number) :
+       pass
+
+def from_dec_to_hex (number) :
        pass
 
 def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     if init_base == 2 and target_base == 2:
-            return bin_to_bin (init_number)
+            return from_bin_to_bin (init_number)
 
     if init_base == 2 and target_base == 10:
-            return bin_to_dec (init_number)
+            return from_bin_to_dec (init_number)
 
     if init_base == 2 and target_base == 16:
-            return bin_to_hex (init_number)
+            return from_bin_to_hex (init_number)
 
     if init_base == 10 and target_base == 2:
-            return dec_to_bin (init_number)
+            return from_dec_to_bin (init_number)
     
     if init_base == 10 and target_base == 10:
-            return dec_to_dec (init_number)            
+            return from_dec_to_dec (init_number)            
     
     if init_base == 10 and target_base == 16:
-            return dec_to_hex (init_number)
+            return from_dec_to_hex (init_number)
 
     if init_base == 16 and target_base == 2:
-            return hex_to_bin (init_number)
+            return from_hex_to_bin (init_number)
 
     if init_base == 16 and target_base == 10:
-            return hex_to_dec (init_number)  
+            return from_hex_to_dec (init_number)  
 
     if init_base == 16 and target_base == 16:
-            return hex_to_hex (init_number)           
+            return from_hex_to_hex (init_number)           
     
     
 
