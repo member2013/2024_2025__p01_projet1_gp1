cdfc07a135af263ad108699dd61397770d996565
member2013 <salomeruiz2008@yahoo.com>
2024-10-18 12:00:02 +0200

    enlever le "from" + def dec_to_bin + def dec_to_hex

--- main.py
+++ main.py
@@ -12,64 +12,147 @@ def transform  ():
     target_number = \
         bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base)
 
-def from_bin_to_bin (number):
-     return number
+def bin_to_bin (init_number):
+     return init_number
 
-def from_dec_to_dec (number):
-     return number
+def dec_to_dec (init_number):
+     return init_number
 
-def from_hex_to_hex (number):
-     return number
+def hex_to_hex (init_number):
+     return init_number
 
-def from_hex_to_bin (number):
-      number = from_hex_to_dec (number)
-      number = from_dec_to_bin (number)
-      return number
+def hex_to_bin (init_number):
+      init_number = hex_to_dec (init_number)
+      init_number = dec_to_bin (init_number)
+      return init_number
 
-def from_bin_to_hex (number) :
-       number = from_bin_to_dec (number)
-       number = from_dec_to_hex (number)
-       return number
+def bin_to_hex (init_number) :
+       init_number = bin_to_dec (init_number)
+       init_number = dec_to_hex (init_number)
+       return init_number
 
-def from_bin_to_dec (number) :
+def bin_to_dec (init_number) :
        pass
 
-def from_dec_to_bin (number) :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def dec_to_bin (init_number) :
+    restes = ""
+    quotient = init_number // 2
+    reste = str (init_number % 2)
+    restes = reste + restes
+    while quotient != 0 :
+        reste = str (quotient % 2)
+        quotient = quotient // 2
+        restes = reste + restes
+    return restes
+
+def hex_to_dec (init_number) :
        pass
 
-def from_hex_to_dec (number) :
-       pass
 
-def from_dec_to_hex (number) :
-       pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def dec_to_hex (init_number) :
+      restes = ""
+      quotient = init_number // 16
+      reste = str (init_number % 16)
+      restes = reste + restes
+      while quotient != 0 :
+        reste = str (quotient % 16)
+        quotient = quotient // 16
+        restes = reste + restes
+        return restes 
+#       ne marche pas encore correctement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     if init_base == 2 and target_base == 2:
-            return from_bin_to_bin (init_number)
+            return bin_to_bin (init_number)
 
     if init_base == 2 and target_base == 10:
-            return from_bin_to_dec (init_number)
+            return bin_to_dec (init_number)
 
     if init_base == 2 and target_base == 16:
-            return from_bin_to_hex (init_number)
+            return bin_to_hex (init_number)
 
     if init_base == 10 and target_base == 2:
-            return from_dec_to_bin (init_number)
+            return dec_to_bin (init_number)
     
     if init_base == 10 and target_base == 10:
-            return from_dec_to_dec (init_number)            
+            return dec_to_dec (init_number)            
     
     if init_base == 10 and target_base == 16:
-            return from_dec_to_hex (init_number)
+            return dec_to_hex (init_number)
 
     if init_base == 16 and target_base == 2:
-            return from_hex_to_bin (init_number)
+            return hex_to_bin (init_number)
 
     if init_base == 16 and target_base == 10:
-            return from_hex_to_dec (init_number)  
+            return hex_to_dec (init_number)  
 
     if init_base == 16 and target_base == 16:
-            return from_hex_to_hex (init_number)           
+            return hex_to_hex (init_number)           
     
     
 
