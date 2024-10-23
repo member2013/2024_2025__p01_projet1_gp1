e303267a572b0b3b5d6f9b7e2a793fe9cefb18d8
member2013 <salomeruiz2008@yahoo.com>
2024-10-23 10:59:38 +0200

    def dec_to_hex (qui fonctionne) + add hex_to_dec de Timéo

--- main.py
+++ main.py
@@ -53,6 +53,9 @@ def bin_to_dec (init_number) :
 
 
 
+
+
+
 def dec_to_bin (init_number) :
     restes = ""
     quotient = init_number // 2
@@ -65,66 +68,41 @@ def dec_to_bin (init_number) :
     return restes
 
 def hex_to_dec (init_number) :
-       pass
+    decimal = 0
+    puissance = 0
+    # On parcourt le nombre hexadécimal de droite à gauche
+    for chiffre in reversed(init_number):
+        if '0' <= chiffre <= '9':
+            valeur = ord(chiffre) - ord('0')  # Convertir caractère '0'-'9' à valeur 0-9
+        elif 'A' <= chiffre <= 'F':
+            valeur = ord(chiffre) - ord('A') + 10  # Convertir 'A'-'F' à valeur 10-15
+        elif 'a' <= chiffre <= 'f':
+            valeur = ord(chiffre) - ord('a') + 10  # Convertir 'a'-'f' à valeur 10-15
+        else:
+            raise ValueError("Caractère hexadécimal invalide")
+        
+        # Calculer la valeur décimale
+        decimal += valeur * (16 ** puissance)
+        puissance += 1
+    
+    return decimal
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# on print
+hexadecimal_input = input("Entrez un nombre hexadécimal : ")
+decimal_output = hex_to_dec(hexadecimal_input)
+print(f"Le nombre en décimal est : {decimal_output}")
 
 def dec_to_hex (init_number) :
-      restes = ""
-      quotient = init_number // 16
-      reste = str (init_number % 16)
-      restes = reste + restes
-      while quotient != 0 :
-        reste = str (quotient % 16)
+    if init_number == 0 :
+        return "0"
+    hex_digits = "0123456789ABCDEF"
+    restes = ""
+    quotient = init_number
+    while quotient > 0 :
+        reste = quotient % 16
+        restes = hex_digits [reste] + restes
         quotient = quotient // 16
-        restes = reste + restes
-        return restes 
-#       ne marche pas encore correctement
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return restes
 
 def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     if init_base == 2 and target_base == 2:
@@ -157,11 +135,6 @@ def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     
 
         
-
-
-
-
-
 
 
 
