a43ea3f16021eb7e197c2648b27cf7b888f636b2
CassandreChanay <cassandrechanay@gmail.com>
2024-11-01 17:09:35 +0100

    final
    tout marche !

--- main.py
+++ main.py
@@ -1,128 +1,111 @@
-from utils import *
+from utils import *  # Assurez-vous que utils.py contient les bonnes fonctions
 
-# def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
-#     target_number = None
-#     return target_number
+def bin_dec_hex__to__bin_dec_hex(init_number, init_base, target_base):
 
+    init_base = int(init_base)
+    target_base = int(target_base)
 
-def transform  ():
+    if init_base == 2 and target_base == 2:
+        return bin_to_bin(init_number)
+
+    if init_base == 2 and target_base == 10:
+        return bin_to_dec(init_number)
+
+    if init_base == 2 and target_base == 16:
+        return bin_to_hex(init_number)
+
+    if init_base == 10 and target_base == 2:
+        return dec_to_bin(init_number)
+
+    if init_base == 10 and target_base == 10:
+        return dec_to_dec(init_number)
+
+    if init_base == 10 and target_base == 16:
+        return dec_to_hex(init_number)
+
+    if init_base == 16 and target_base == 2:
+        return hex_to_bin(init_number)
+
+    if init_base == 16 and target_base == 10:
+        return hex_to_dec(init_number)
+
+    if init_base == 16 and target_base == 16:
+        return hex_to_hex(init_number)
+    
+    return None
+
+def transform():
     init_number = ask_for_init_number()
     init_base = ask_for_init_base()
     target_base = ask_for_target_base()
-    target_number = \
-        bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base)
+    
+    final_number = bin_dec_hex__to__bin_dec_hex(init_number, init_base, target_base)
+    
+    print(f"Le nombre converti est : {final_number}")
 
-def bin_to_bin (init_number):
-     return init_number
+def bin_to_bin(init_number):
+    return init_number
 
-def dec_to_dec (init_number):
-     return init_number
+def dec_to_dec(init_number):
+    return init_number
 
-def hex_to_hex (init_number):
-     return init_number
+def hex_to_hex(init_number):
+    return init_number
 
-def hex_to_bin (init_number):
-      init_number = hex_to_dec (init_number)
-      init_number = dec_to_bin (init_number)
-      return init_number
+def hex_to_bin(init_number):
+    init_number = hex_to_dec(init_number)
+    return dec_to_bin(init_number)
 
-def bin_to_hex (init_number) :
-       init_number = bin_to_dec (init_number)
-       init_number = dec_to_hex (init_number)
-       return init_number
+def bin_to_hex(init_number):
+    init_number = bin_to_dec(init_number)
+    return dec_to_hex(init_number)
 
-def bin_to_dec (init_number) :
+def bin_to_dec(init_number):
     result = 0
     exposant = len(init_number) - 1
     for char in init_number:
         if char == '1':
             result += 2 ** exposant
         exposant -= 1
-    
     return result
 
-def dec_to_bin (init_number) :
-    init_number = int (init_number)
+def dec_to_bin(init_number):
+    init_number = int(init_number)
+    if init_number == 0:
+        return "0"
     restes = ""
-    quotient = init_number // 2
-    reste = str (init_number % 2)
-    restes = reste + restes
-    while quotient != 0 :
-        reste = str (quotient % 2)
-        quotient = quotient // 2
-        restes = reste + restes
+    while init_number > 0:
+        restes = str(init_number % 2) + restes
+        init_number //= 2
     return restes
 
-def hex_to_dec (init_number) :
+def hex_to_dec(init_number):
     decimal = 0
     puissance = 0
-    # On parcourt le nombre hexadécimal de droite à gauche
     for chiffre in reversed(init_number):
         if '0' <= chiffre <= '9':
-            valeur = ord(chiffre) - ord('0')  # Convertir caractère '0'-'9' à valeur 0-9
+            valeur = ord(chiffre) - ord('0')
         elif 'A' <= chiffre <= 'F':
-            valeur = ord(chiffre) - ord('A') + 10  # Convertir 'A'-'F' à valeur 10-15
+            valeur = ord(chiffre) - ord('A') + 10
         elif 'a' <= chiffre <= 'f':
-            valeur = ord(chiffre) - ord('a') + 10  # Convertir 'a'-'f' à valeur 10-15
+            valeur = ord(chiffre) - ord('a') + 10
         else:
             raise ValueError("Caractère hexadécimal invalide")
-        
-        # Calculer la valeur décimale
         decimal += valeur * (16 ** puissance)
         puissance += 1
-    
     return decimal
 
-
-def dec_to_hex (init_number) :
-    if init_number == 0 :
+def dec_to_hex(init_number):
+    init_number = int(init_number)
+    if init_number == 0:
         return "0"
     hex_digits = "0123456789ABCDEF"
     restes = ""
     quotient = init_number
-    while quotient > 0 :
+    while quotient > 0:
         reste = quotient % 16
-        restes = hex_digits [reste] + restes
-        quotient = quotient // 16
-    return restes
-
-def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
-    if init_base == 2 and target_base == 2:
-            return bin_to_bin (init_number)
-
-    if init_base == 2 and target_base == 10:
-            return bin_to_dec (init_number)
-
-    if init_base == 2 and target_base == 16:
-            return bin_to_hex (init_number)
-
-    if init_base == 10 and target_base == 2:
-            return dec_to_bin (init_number)
-    
-    if init_base == 10 and target_base == 10:
-            return dec_to_dec (init_number)            
-    
-    if init_base == 10 and target_base == 16:
-            return dec_to_hex (init_number)
-
-    if init_base == 16 and target_base == 2:
-            return hex_to_bin (init_number)
-
-    if init_base == 16 and target_base == 10:
-            return hex_to_dec (init_number)  
-
-    if init_base == 16 and target_base == 16:
-            return hex_to_hex (init_number)           
-    
-
-
-        
-
+        restes = hex_digits[reste] + restes
+        quotient //= 16
+    return restes        
 
 transform()
-
-
-
-# assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
-
-# ask_for_the_init_number 144
--- utils.py
+++ utils.py
@@ -19,13 +19,13 @@ def is_a_valid_base(base):
     return base in valid_base_strings
 
 def ask_for_init_base():
-    init_base = input(ask_for_the_init_base_text())
+    init_base = input(ask_for_the_init_base_text)
     while not is_a_valid_base(init_base):
-        init_base = input(ask_again_for_the_init_base_text())
+        init_base = input(ask_again_for_the_init_base_text)
     return init_base
 
 def ask_for_target_base():
-    target_base = input(ask_for_the_target_base_text())
+    target_base = input(ask_for_the_target_base_text)
     while not is_a_valid_base(target_base):
-        target_base = input(ask_again_for_the_target_base_text())
+        target_base = input(ask_again_for_the_target_base_text)
     return target_base
